8fff5ae3a7a68134306e41bd54ef531b68e4e3c6
tinatries <60852586+tinatries@users.noreply.github.com>
2022-03-04 23:34:47 -0500

    Main + Output Updates
    Main: Finished if the letter has already been guessed or not
    
    Output: tried to do a if like if the key is not a letter than it will say to enter a letter..no idea if it works

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    //instantiating object(s)
+    Output_Panel gui = new Output_Panel();
+
     public static void main(String[] args) throws Exception {
         //array where all the words are going to be
         String[] wordsArr = new String[500];
@@ -29,14 +32,33 @@ public class Main {
     }
 
     public void game() {
+    }
+
+    public char letterGuessed() {
         //guessed letters (26, because 26 letters)
+        char letterGuessed = gui.returnUserInput();
         char[] lettersGuessed = new char[26];
         boolean alreadyGuessed = false;
 
         //checks if the letter has already been guessed
-        for (int i = 0; i < lettersGuessed.length; i++)
-        {
-
+        for (int i = 0; i < lettersGuessed.length; i++) {
+            if (lettersGuessed.equals(lettersGuessed[i])) {
+                i = lettersGuessed.length;
+                System.out.println("already chosen");
+                alreadyGuessed = true;
+            }
         }
+
+        if (alreadyGuessed ==  false) {
+            for (int i = 0; i < lettersGuessed.length; i++) {
+                if (lettersGuessed[i] == 0) {
+                    lettersGuessed[i] = letterGuessed;
+                    return letterGuessed;
+                }
+            }
+        }
+        return 0;
     }
+
+
 }
--- src/Output_Panel.java
+++ src/Output_Panel.java
@@ -4,7 +4,7 @@ import java.awt.event.KeyListener;
 import javax.swing.*;
 
 
-public abstract class Output_Panel extends JPanel implements KeyListener {
+public class Output_Panel extends JPanel implements KeyListener {
     //constructor: makes the panel/gui and also adds the keylistener to that said panel
     public Output_Panel() {
         //these lines are settings for the panel
@@ -15,22 +15,37 @@ public abstract class Output_Panel extends JPanel implements KeyListener {
         this.addKeyListener(this);
     }
 
-    //find character pressed using KeyListener and returns it
     char userinput;
+
+    //find character pressed using KeyListener and returns it
     @Override
     public void keyTyped(KeyEvent e) {
         userinput = e.getKeyChar();
-        returnUserInput();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        boolean isLetter = false;
+        for (int i = 'a'; i <= 'z'; i++)
+        {
+            if(e.getKeyCode() == i)
+            {
+                isLetter = true;
+            }
+        }
+
+        if (isLetter == false) {
+            System.out.println("enter a letter");
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.VK_RIGHT)
-            System.out.println("You pressed the right arrow");
-        this.repaint();
+        //this is there so that the class actually runs without being an abstract...
     }
 
-    //Just in case;
+    //Methods for Main
     public char returnUserInput()
     {
         System.out.println("The key you pressed was " + userinput );
